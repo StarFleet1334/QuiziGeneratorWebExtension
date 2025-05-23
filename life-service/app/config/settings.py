e3a1540bc6d7e957f76aa54fa591e3a6579b6fb6
Ilia <latariailia6@gmail.com>
2025-05-23 15:43:21 -0700

    some minor fix

--- life-service/app/config/settings.py
+++ life-service/app/config/settings.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    API_KEY: str = "AIzaSyDiWFHBDzyG5WKtMkTbHsS7Bcl5OugBbQM"
+    API_KEY: str = "<API_KEY>"
     MODEL_NAME: str = "gemini-2.0-flash"
     MAX_CHUNK_LENGTH: int = 200
 
